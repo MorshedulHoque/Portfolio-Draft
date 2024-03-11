cd98dbcca020c95701d2d822ca264d3a516ee49d
taibislamdipu <mailtaibislam@gmail.com>
2024-03-11 21:53:22 +0600

    ui: on progress.

--- app/page.js
+++ app/page.js
@@ -2,12 +2,14 @@ import Educations from "@/components/Educations";
 import Experiences from "@/components/Experiences";
 import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
+import Navbar from "@/components/Navbar";
 import Projects from "@/components/Projects";
 import Skills from "@/components/Skills";
 
 export default function Home() {
   return (
     <div className="space-y-20">
+      <Navbar />
       <Hero />
       <Experiences />
       <Projects />
